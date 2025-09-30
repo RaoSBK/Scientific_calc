51baa4014a1b93f6712b5a5f351be043a9875eab
Suraj Bhan Kumar <surajbhan20005@gmail.com>
2025-09-30 22:32:21 +0530

    upto CE button

--- src/script.js
+++ src/script.js
@@ -1,53 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {
-                console.log("Document is ready:");
-                const display = document.getElementById('calc-display');
-                const buttons = document.getElementsByClassName('btn');
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready:");
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
 
-                let currentValue = "";
+    let currentValue = "";
 
-                function evaluateResult () {
-                    console.log('currentValue:', currentValue)
-                    const convertedValue = currentValue
-                    .replace("×", "*")
-                    .replace("÷" , "/")
-                    .replace('%', "*0.01")
-                    .replace('sin', 'Math.sin')
-                    .replace('cos', 'Math.cos')
-                    .replace('tan', 'Math.tan')
-                    .replace('ln', 'Math.log')
-                    .replace('π', 'Math.PI')
-                    .replace('log', 'Math.log10')
-                    .replace('e', 'Math.E')
-                    .replace('√', 'Math.sqrt')
-                    .replace('CE', 'delete');
-                    console.log('currentValue:', convertedValue)
-                    const result = eval(convertedValue);
-                    currentValue = result.toString();
+    function evaluateResult() {
+        console.log('currentValue:', currentValue)
+        const convertedValue = currentValue
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace('%', "*0.01")
+            .replace('sin', 'Math.sin')
+            .replace('cos', 'Math.cos')
+            .replace('tan', 'Math.tan')
+            .replace('ln', 'Math.log')
+            .replace('π', 'Math.PI')
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace('√', 'Math.sqrt')
+            .replace('CE', 'delete')
+            .replace('CE', 'delete');
+        console.log('currentValue:', convertedValue)
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue;
+    }
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
+
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value == "CE") {
+                    currentValue = currentValue.slice(0, -1);
+                    display.value = currentValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
                     display.value = currentValue;
                 }
-                for(let i=0; i < buttons.length; i++){
-                    const button = buttons[i];
-                    button.addEventListener('click', function (){
-                        const value = button.innerText;
+            } catch (error) {
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }
 
-                        try {
-                                if(value == "AC"){
-                                currentValue = "";
-                                display.value = currentValue;
-                            } else if(value == "="){
-                                evaluateResult();
-                            } else {
-                                currentValue += value;
-                                display.value = currentValue;
-                            }
-                        } catch (error) {
-                            console.error(error);
-                            currentValue = "ERROR";
-                            display.value = currentValue;
-                        }
 
-                        
-                    })
-                }
+        })
+    }
 
-            });
+});
